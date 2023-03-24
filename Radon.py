e6a55234068ff28765445b28b687d97ec4a45d48
Szymon-Winiecki <Szymon.win4@gmail.com>
2023-03-24 15:41:27 +0100

    optimization

--- Radon.py
+++ Radon.py
@@ -1,11 +1,14 @@
 from PIL import ImageTk, Image, ImageDraw, ImageOps
 import numpy as np
+import time
 
 class Radon:
 
     def __init__(self, baseImage, startRotation = 0, numberOfEmitters = 10, emittersAngularSpan = 1.57075, rotationDelta = 0.04363):
         
         self.baseImage = baseImage
+        self.baseImageL = baseImage.convert('L')
+        self.baseImageArray = np.array(self.baseImageL)
 
         self.configAndReset(startRotation=startRotation, numberOfEmitters=numberOfEmitters, emittersAngularSpan=emittersAngularSpan, rotationDelta=rotationDelta)
 
@@ -46,6 +49,9 @@ class Radon:
 
     #calculates sinogram from 'from_iteration' inclusive to 'to_iteration' exclusive
     def generateSinogram(self, from_iteration=None, to_iteration=None):
+
+        s_time = time.time()
+
         if from_iteration == None:
             from_iteration = self.currentIteration
         if to_iteration == None:
@@ -54,42 +60,31 @@ class Radon:
         if from_iteration >= self.numberOfIterations or to_iteration > self.numberOfIterations:
             return
 
+        #current roation of all emitters (indices from 0 to self.numberOfEmitters - 1) and detectors (indices from self.numberOfEmitters to 2*self.numberOfEmitters - 1) in radians
+        rotation = np.arange(self.numberOfEmitters)
+        rotation = np.concatenate((rotation, rotation))
+
+        initRotation = self.startRotation + (from_iteration - 1) * self.rotationDelta - self.halfOfSpan
+        rotation = rotation * self.angleGapBetweenSensors + initRotation
+        rotation[self.numberOfEmitters : ] += np.pi
+
         for iter in range(from_iteration, to_iteration):
-            rotation = self.startRotation + iter * self.rotationDelta
-            rotationSym = rotation + np.pi
-            #emitters
+            rotation += self.rotationDelta
+
+            x = self.scannerRadius * np.cos(rotation) + self.imageCenter[0]
+            y = self.scannerRadius * np.sin(rotation) + self.imageCenter[1]
+
+            last = len(x) - 1
+            #emitters and detectors
             for i in range(self.numberOfEmitters):
-                self.angles[i] = rotation - self.halfOfSpan + i * self.angleGapBetweenSensors
+                points = self.bresenham_integer(int(x[i]), int(y[i]), int(x[last - i]), int(y[last - i]))
+                self.radonmatrix[i, iter] = self.sumPixels(points, self.baseImageArray)
 
-            #detectors
-            for i in range(self.numberOfEmitters):
-                self.angles[i + self.numberOfEmitters] = rotationSym - self.halfOfSpan + i * self.angleGapBetweenSensors
-
-            cos = np.cos(self.angles)
-            sin = np.sin(self.angles)
-
-            x = self.scannerRadius * cos + self.imageCenter[0]
-            y = self.scannerRadius * sin + self.imageCenter[1]
-
-            pairs=[]
-            #emitters
-            for i in range(self.numberOfEmitters):
-                pairs.append([int(x[i]),int(y[i])])
-            #detectors
-            temp=0
-            for i in range(len(x) - 1, self.numberOfEmitters - 1, -1):
-                pairs[temp].append(int(x[i]))
-                pairs[temp].append(int(y[i]))
-                temp+=1
-
-            row = 0
-            for i in pairs:
-                points = self.bresenham_integer(*i)
-                self.radonmatrix[row, iter] = self.sumPixels(points, self.baseImage)
-                row += 1
             self.currentIteration += 1
         
-        self.radonmatrixNorm = 255 * self.radonmatrix / np.max(self.radonmatrix)
+        self.radonmatrixNorm = self.radonmatrix / (np.max(self.radonmatrix) / 255)
+
+        print(time.time() - s_time)
 
     def nextIteration(self, count=1):
         self.generateSinogram(to_iteration=self.currentIteration + count + 1)
@@ -108,9 +103,15 @@ class Radon:
 
         err = dx - dy
 
-        points = []
+        w =  self.baseImage.size[0]
+        h =  self.baseImage.size[1]
+
+        x = []
+        y = []
         while x0 != x1 or y0 != y1:
-            points.append((x0, y0))
+            if x0 > 0 and x0 < w and y0 > 0 and y0 < h:
+                x.append(x0)
+                y.append(y0)
             e2 = err * 2
             if e2 > -dy:
                 err -= dy
@@ -119,16 +120,11 @@ class Radon:
                 err += dx
                 y0 += sy
 
-        points.append((x0, y0))
-        return points
+        if x0 > 0 and x0 < w and y0 > 0 and y0 < h:
+            x.append(x0)
+            y.append(y0)
+        return [x, y]
 
 
-    def sumPixels(self, points, image): # sumowanie wartości pikseli w linii
-        imgWidth = image.size[0]
-        imgHeight = image.size[1]
-        image = np.array(image.convert('L'))
-        res = 0
-        for point in points:
-            if point[0] > 0 and point[0] < imgWidth and point[1] > 0 and point[1] < imgHeight:
-                res+=image[point[1], point[0]]
-        return res
+    def sumPixels(self, points, imageArray): # sumowanie wartości pikseli w linii
+        return np.sum(imageArray[points[0], points[1]])
